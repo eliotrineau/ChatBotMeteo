7d616c4e9cbb62dd567d9c4d1456fb6057cc6bbc
Anton Adam <116871473+aent0n@users.noreply.github.com>
2024-04-08 09:36:39 +0000

    Co-authored-by: eliotrineau <eliotrineau@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #b09e3c93acf17d44ab9a805b88b2a074 //API KEY
 
 
+import requests as rq
+
+cityInput = input("Veuillez rentrer un nom de ville")
+langue = "fr"
+apiKey = "b09e3c93acf17d44ab9a805b88b2a074"
+apiLink = f"https://api.openweathermap.org/data/2.5/weather?q={cityInput}&appid={apiKey}&lang={langue}"
+
+
+json = rq.get(apiLink).json
+JSON = json['weather'][0]['description']
+
+print(JSON)
