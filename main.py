aa5c126a32294c23ec7a9e0fb0ac74f2b1fc6ff7
Anton Adam <116871473+aent0n@users.noreply.github.com>
2024-04-08 09:43:37 +0000

    Co-authored-by: eliotrineau <eliotrineau@users.noreply.github.com> Co-authored-by: Belaid IOUDJAOUDENE <belaid226@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 
 
 import requests as rq
+import json
 
+file = open('outfits.json')
+outfitsData = json.load(file)
+
+sexe = input("Êtes vous un homme ou une femme ?\n0 : femme\n1 : homme")
+styleVestimentaire = input("Veuillez faire votre choix vestimentaire :\n1 : été\n2 : automne\n3 : hiver\n 4 : printemps")
 cityInput = input("Veuillez rentrer un nom de ville")
 langue = "fr"
 apiKey = "b09e3c93acf17d44ab9a805b88b2a074"
 apiLink = f"https://api.openweathermap.org/data/2.5/weather?q={cityInput}&appid={apiKey}&lang={langue}"
 
 
-json = rq.get(apiLink).json
+json = rq.get(apiLink).json()
 JSON = json['weather'][0]['description']
 
+file.close()
 print(JSON)
