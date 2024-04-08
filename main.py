1d639777246221d27d2ebe0b9a8d107368685e9a
Anton Adam <116871473+aent0n@users.noreply.github.com>
2024-04-08 12:25:55 +0000

    Mise à jour de l'interface du Chatbot

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import requests as rq
 import json as json_module
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 def sexe():
     os.system('clear')
